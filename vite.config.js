37ace2e7e2e4576162540ed992c9e3a7376343b5
Matteo Leclercq _1 <matteo.ftt@gmail.com>
2023-10-20 10:35:03 +0900

    deploy: create github pages to deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/xr-scene-generator',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
