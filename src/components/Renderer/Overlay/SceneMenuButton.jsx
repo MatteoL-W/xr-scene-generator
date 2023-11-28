141853dddc944225fa580ba0282c1fa2eea06643
Matteo Leclercq _1 <matteo.ftt@gmail.com>
2023-11-28 14:47:37 +0900

    core: add a delete button to focused mesh modifier

--- src/components/Renderer/Overlay/SceneMenuButton.jsx
+++ src/components/Renderer/Overlay/SceneMenuButton.jsx
@@ -8,8 +8,8 @@ export default function SceneMenuButton() {
       className='absolute top-5 left-5 p-2 flex flex-col z-10 bg-jean text-white rounded cursor-pointer'
       onClick={() => setMenuState(true)}
     >
-      <span className='text-center text-5xl font-semibold'>+</span>
-      Add an object
+      <span className='text-center text-4xl font-semibold'>+</span>
+      Add object
     </div>
   )
 }
--- src/components/RightBar/Modifier/index.jsx
+++ src/components/RightBar/Modifier/index.jsx
@@ -4,19 +4,30 @@ import { BsFillBoxFill } from 'react-icons/bs'
 import ModifierInputs from './ModifierInputs.jsx'
 import { useState } from 'react'
 import ModifierTitle from '@/components/RightBar/Modifier/ModifierTitle.jsx'
+import { AiFillDelete } from 'react-icons/ai'
+import useStore from '@/store/index.jsx'
 
 export default function ObjectModifier() {
   const focusedObject = useFocusedObjectData()
   const [modifierMode, setModifierMode] = useState('geometry')
+  const [removeObject] = useStore((state) => [state.removeObjectFromScene])
 
   if (!focusedObject) return
 
   return (
     <div className='border-b border-b-white'>
-      <Title
-        title={`"${focusedObject.userSetName}" Modifier`}
-        Icon={BsFillBoxFill}
-      />
+      <div className='relative'>
+        <Title
+          title={`"${focusedObject.userSetName}" Modifier`}
+          Icon={BsFillBoxFill}
+        />
+        <div className='absolute top-0 right-2 h-full flex items-center text-xl'>
+          <AiFillDelete
+            className='cursor-pointer'
+            onClick={() => removeObject(focusedObject)}
+          />
+        </div>
+      </div>
 
       <ModifierTitle
         focusedObject={focusedObject}
