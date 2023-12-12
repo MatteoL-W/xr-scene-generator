5b04df1901800a48f6232b249e31fa3e1614eeff
Matteo Leclercq _1 <matteo.ftt@gmail.com>
2023-12-12 16:57:33 +0900

    core: focus new added object

--- src/components/Renderer/Canvas/SceneObjects/useSyncThreeAndInternal.jsx
+++ src/components/Renderer/Canvas/SceneObjects/useSyncThreeAndInternal.jsx
@@ -3,17 +3,23 @@ import useThreeObject from '@/hooks/useThreeObject.jsx'
 import { useEffect } from 'react'
 
 export default function useSyncThreeAndInternal(object, objectComponentRef) {
-  const [modifyObjectUUID] = useStore((state) => [state.modifyObjectUUID])
+  const [modifyObjectUUID, changeFocusedObjectByUUID] = useStore((state) => [
+    state.modifyObjectUUID,
+    state.changeFocusedObjectByUUID,
+  ])
   const threeObject = useThreeObject(object.uuid)
 
   useEffect(() => {
     // Attach the three.js object to this object template if it's imported
     if (object.isImported && threeObject) {
       objectComponentRef?.current?.attach(threeObject)
+      changeFocusedObjectByUUID(threeObject.uuid)
       return
     }
 
     // Else set the object uuid the same as R3F Object uuid
     modifyObjectUUID(object, objectComponentRef?.current?.uuid)
+
+    changeFocusedObjectByUUID(objectComponentRef?.current?.uuid)
   }, []) // (do not add more dependencies)
 }
